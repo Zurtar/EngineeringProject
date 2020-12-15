c8582a1c5a5148b3b09f021aff52ecb352c9e560
Zurtar <ethan.cook3552@gmail.com>
2020-12-15 12:08:44 -0500

    Real World Changes BB!

--- src/devices.cpp
+++ src/devices.cpp
@@ -1,23 +1,28 @@
 #include "devices.h"
 
+Motor leftMotorA(1);
+Motor leftMotorB(3);
+Motor rightMotorA(-2);
+Motor rightMotorB(-4);
+
 //Declaring these sepreately so I can so some debugging without having to get the values through our chassis
 ADIEncoder leftEncoder = ADIEncoder('A','B');
-ADIEncoder rightEncoder = ADIEncoder('C','D', true);
-ADIEncoder backEncoder = ADIEncoder('E','F');
+ADIEncoder rightEncoder = ADIEncoder('E','F',true);
+ADIEncoder backEncoder = ADIEncoder('C','D');
 
 //setting up our chassis, we should add a PID controller later with the .WithGains() method
 std::shared_ptr<okapi::OdomChassisController> chassis = okapi::ChassisControllerBuilder()
           //1,2 are our left motors -3,-4 are the right motors so they are reserved
-          .withMotors({1,2}, {-3,-4})
+          .withMotors({leftMotorA,leftMotorB}, {rightMotorA,rightMotorB})
           //Declaring the dimensions of our wheels these values need to be adjusted, also declares gearset
-          .withDimensions(AbstractMotor::gearset::green, {{4_in,11.5_in},imev5GreenTPR})
+          .withDimensions(AbstractMotor::gearset::green, {{4_in,12.25_in},imev5GreenTPR})
           //setting up our tracking encoders
           .withSensors(leftEncoder, rightEncoder, backEncoder)
           .withOdometry({
             //This is the size of our left and right tracking wheels
-              {2.75_in,7_in,
+              {4.16_in,12.25_in,
               //Our back tracking wheel
-              1_in,2.75_in},
+              8.25_in,12.25_in},
               //Constant for the ticks per rotation for our encoder
               quadEncoderTPR},
               //Our coordinates system for odom
--- src/main.cpp
+++ src/main.cpp
@@ -60,7 +60,6 @@ void competition_initialize() {}
  * from where it left off.
  */
 void autonomous() {
-	chassis->turnToAngle(90_deg);
 }
 
 /**
@@ -77,19 +76,7 @@ void autonomous() {
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
-
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
-
-		left_mtr = left;
-		right_mtr = right;
-		pros::delay(20);
+	chassis->turnToAngle(360_deg);
+	chassis->waitUntilSettled();
+		pros::delay(2000);
 	}
-}
